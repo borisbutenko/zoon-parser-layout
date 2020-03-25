9098bd5b7e6d88c7a50125a7b1a19270722987bf
mq <iamborisbutenko@gmail.com>
2020-03-26 00:06:56 +0300

    .

--- server/index.py
+++
@@ -1,7 +0,0 @@
-from http.server import SimpleHTTPRequestHandler
-from socketserver import TCPServer
-
-
-with TCPServer(("", 8080), SimpleHTTPRequestHandler) as httpd:
-	print(f"serving at port {8080}")
-	httpd.serve_forever()
